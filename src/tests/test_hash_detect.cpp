0e22cc8eab1ffba0462e315d734a80fa88295936
bnb <home@hackbookpro.lan>
2026-04-15 01:33:38 -0400

    fix

--- src/tests/test_hash_detect.cpp
+++ src/tests/test_hash_detect.cpp
@@ -6,30 +6,29 @@
 #include "../include/Normalize.hpp"
 #include "../include/Parser.hpp"
 
-int main(int argc, char* argv[])
-{
-    if (argc < 2) {
-        std::cerr << "Usage: ./detector <file.cpp>\n";
-        return 1;
-    }
+int main(int argc, char* argv[]) {
+	if (argc < 2) {
+		std::cerr << "Usage: ./detector <file.cpp>\n";
+		return 1;
+	}
 
-    std::ifstream file1(argv[1]);
+	std::ifstream file1(argv[1]);
 
-    if (!file1) {
-        std::cerr << "Error opening input file\n";
-        return 1;
-    }
+	if (!file1) {
+		std::cerr << "Error opening input file\n";
+		return 1;
+	}
 
-    // Step 1: normalize the file
-    std::vector<std::string> tokens1 = normalize(file1);
+	// Step 1: normalize the file
+	std::vector<std::string> tokens1 = normalize(file1);
 
-    // Step 2: parse functions
-    std::vector<Function> funcs1 = parse_functions(tokens1);
+	// Step 2: parse functions
+	auto funcs1 = parse_functions(argv[1], tokens1);
 
-    // Step 3: detect clones within the same file
-    int count = detectType1Clones(funcs1);
+	// Step 3: detect clones within the same file
+	int count = detectType1Clones(funcs1.first);
 
-    std::cout << "Total amount of Type-1 Clones found: " << count << std::endl;
+	std::cout << "Total amount of Type-1 Clones found: " << count << std::endl;
 
-    return 0;
+	return 0;
 }
